45837475216d8fb6f2e22198928df06bc4806e82
Rund <rundcodehero@gmail.com>
2022-04-07 20:17:33 +0700

    add new file to new repo

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "admin";
+$db_pass = "rundcode";
+$db_name = "dbsosmed";
+
+try {    
+    //create PDO connection 
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
+}
