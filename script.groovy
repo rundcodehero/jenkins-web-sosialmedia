0807dd919c52d67cdf1ac32c4418848b18237f26
Rund <rundcodehero@gmail.com>
2022-04-08 20:43:53 +0700

    ..

--- script.groovy
+++ script.groovy
@@ -4,15 +4,14 @@ def testEnv() {
     sh 'php --version'
 }
 
-/* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
+
 def buildImage() {
     echo 'building sosmed image..'
     withCredentials [usernamePassword(credentialsID: 'hub-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]
         sh 'docker build -t rundcode/jenkins-sosialmedia:v1.0 .'
 }
-buildImage
+
 def pushImage() {
-    /* groovylint-disable-next-line DuplicateMapLiteral, DuplicateStringLiteral */
     withCredentials [usernamePassword(credentialsID: 'hub-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]
       sh "echo $PASS | docker login -u $USER --password-stdin"
       sh 'docker push rundcode/jenkins-sosialmedia:v1.0'
