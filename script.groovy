05d2f8f7df74e99cb9bc29daa7c97970c819125d
Rund <rundcodehero@gmail.com>
2022-04-08 22:04:26 +0700

    ..

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     /* groovylint-disable-next-line LineLength */
     withCredentials {[usernamePassword (credentialsID: 'hub-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]}
         sh 'docker build -t rundcode/jenkins-sosialmedia:v1.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
         sh 'docker push rundcode/jenkins-sosialmedia:v1.0'
 }
 
