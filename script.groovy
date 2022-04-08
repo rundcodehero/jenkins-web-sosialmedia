6e3ff33f0aa38722a5688086b6f35369f0ec54d6
Rund <rundcodehero@gmail.com>
2022-04-08 21:06:40 +0700

    ..

--- script.groovy
+++ script.groovy
@@ -1,7 +1,3 @@
-/* groovylint-disable FactoryMethodName, LineLength */
-import jenkins.model.*
-jenkins = Jenkins.instance
-
 def testEnv() {
     sh 'docker --version'
     sh 'php --version'
@@ -9,13 +5,11 @@ def testEnv() {
 
 
 def buildImage() {
-    echo 'building sosmed image..'
-    withCredentials [usernamePassword(credentialsID: 'hub-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]
+     withCredentials [usernamePassword(credentialsID: 'hub-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]
         sh 'docker build -t rundcode/jenkins-sosialmedia:v1.0 .'
 }
 
 def pushImage() {
-    /* groovylint-disable-next-line DuplicateMapLiteral, DuplicateStringLiteral */
       sh "echo $PASS | docker login -u $USER --password-stdin"
       sh 'docker push rundcode/jenkins-sosialmedia:v1.0'
 }
