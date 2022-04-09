a028f73fec1c6183145313ad4b0a0560be29eb38
Rund <rundcodehero@gmail.com>
2022-04-09 08:20:40 +0700

    ..

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def testEnv() {
 }
 
 def buildImage() {
-    sh "echo 'lets build Docker Image'"
+    sh "echo 'lets BUILD Docker Image'"
     sh 'docker build -t rundcode/jenkins-sosialmedia:v1.0 .'
     sh 'docker rmi $(docker images --filter "dangling=true" -q)'
 }
@@ -16,12 +16,14 @@ def pushImage() {
 
 def pushImage2() {
     /* groovylint-disable-next-line LineLength */
-    withCredentials([usernamePassword(credentialsId: 'hub-docker-repo', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+    sh "echo 'lets PUSH Docker Image'"
+    withCredentials([usernamePassword(credentialsId: 'hub-docker-repo', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')])
           sh "echo ${env.dockerHubPassword} | docker login -u ${env.dockerHubUser} --password-stdin"
           sh 'docker push rundcode/jenkins-sosialmedia:v1.0'
 }
 
-def runImage() {
+def runImage () {
+    sh "echo 'lets TEST RUN Image'"
     sh 'docker stop sosmed'
     sh 'docker rm sosmed'
     sh 'docker run -dit -p 8000:80 --name sosmed rundcode/jenkins-sosialmedia:v1.0'
